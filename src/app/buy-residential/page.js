e9a54ec4a874545151ba9e139a5761035059c97d
Masih DR <masih.do2015@gmail.com>
2025-09-27 21:37:10 +0330

    change link

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,25 +1,25 @@
 import BuyResidentialPage from "@/components/templates/BuyResidentialPage";
 
-export const dynamic = "force-dynamic"; // اضافه شد
-
 async function page({ searchParams }) {
   const params = searchParams;
 
-  // حتما cache: 'no-store' بذاریم تا SSR همیشه جدید بده
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/profile`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://real-state-dusky-seven.vercel.app/api/profile`,
+    {
+      cache: "no-store",
+    }
+  );
 
   const data = await res.json();
 
   if (data.error) {
     return <h3>مشکلی پیش آمده است</h3>;
-  } 
+  }
 
   let finalData = data.data;
 
   if (params?.category) {
-    finalData = finalData.filter(i => i.category === params.category);
+    finalData = finalData.filter((i) => i.category === params.category);
   }
 
   return <BuyResidentialPage data={finalData} />;
